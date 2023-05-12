65397c35fb487469ed4421de18701eb37bbbbd45
Maksym Ostapchuk <gadget.add@gmail.com>
2023-05-12 09:19:07 +0300

    update RTK Query to axiosBaseQuery

--- src/redux/contactsApi.js
+++ src/redux/contactsApi.js
@@ -1,33 +1,52 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi } from '@reduxjs/toolkit/query/react'
+import axios from 'axios'
+
+const axiosBaseQuery =
+    ({ baseUrl } = { baseUrl: '' }) =>
+        async ({ url, method, data, params }) => {
+            try {
+                const result = await axios({ url: baseUrl + url, method, data, params })
+                return { data: result.data }
+            } catch (axiosError) {
+                let err = axiosError
+                return {
+                    error: {
+                        status: err.response?.status,
+                        data: err.response?.data || err.message,
+                    },
+                }
+            }
+        }
 
 export const contactsApi = createApi({
     reducerPath: 'contactsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://645897178badff578ef539ed.mockapi.io' }),
+    baseQuery: axiosBaseQuery({ baseUrl: 'https://645897178badff578ef539ed.mockapi.io' }),
     tagTypes: ['Contacts'],
     endpoints: (builder) => ({
         fetchContacts: builder.query({
-            query: () => "/contacts",
+            query: () => ({
+                url: "/contacts",
+                method: 'GET',
+            }),
             providesTags: (result) => (result
                 ? [...result.map(({ id }) => ({ type: 'Contacts', id })), { type: 'Contacts', id: 'LIST' }]
                 : [{ type: 'Contacts', id: 'LIST' }]),
         }),
         addContact: builder.mutation({
-            query(body) {
-                return {
-                    url: "/contacts",
-                    method: 'POST',
-                    body,
-                }
-            },
+            query: (data) => ({
+                url: "/contacts",
+                method: 'POST',
+                data,
+            })
+            ,
             invalidatesTags: [{ type: 'Contacts', id: 'LIST' }],
         }),
         deleteContact: builder.mutation({
-            query(id) {
-                return {
-                    url: `/contacts/${id}`,
-                    method: 'DELETE',
-                }
-            },
+            query: (id) => ({
+                url: `/contacts/${id}`,
+                method: 'DELETE',
+            })
+            ,
             invalidatesTags: [{ type: 'Contacts', id: 'LIST' }]
         })
     }),
